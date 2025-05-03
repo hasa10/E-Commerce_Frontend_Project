8b1fac228e0a946a9242ff4678e9e44b34f16a71
hasa10 <rasitha8000@gmail.com>
2025-05-03 11:38:02 +0530

    created app.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MatSlideToggleModule,MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
