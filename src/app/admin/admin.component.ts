f0f245047de73fc3b0e67c61badfecc5765d3f33
hasa10 <rasitha8000@gmail.com>
2025-05-10 08:56:16 +0530

    create admin service

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule,ReactiveFormsModule,HttpClientModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
