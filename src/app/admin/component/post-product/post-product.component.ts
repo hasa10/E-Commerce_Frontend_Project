e968c2a1545c93b72b33014c9877e7199ff3dbe0
hasa10 <rasitha8000@gmail.com>
2025-05-16 14:19:39 +0530

    created post-product component

--- src/app/admin/component/post-product/post-product.component.ts
+++ src/app/admin/component/post-product/post-product.component.ts
@@ -1,9 +1,16 @@
+import { NgIf, CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatOptionModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-post-product',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule,MatInputModule,ReactiveFormsModule,NgIf,MatIconModule,MatOptionModule,MatSelectModule,CommonModule],
   templateUrl: './post-product.component.html',
   styleUrl: './post-product.component.css'
 })
