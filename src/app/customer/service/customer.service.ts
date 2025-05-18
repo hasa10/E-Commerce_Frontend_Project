e9788d41375ef64229264564f9ac57a9e415f724
hasa10 <rasitha8000@gmail.com>
2025-05-18 07:54:27 +0530

    updated customer service method

--- src/app/customer/service/customer.service.ts
+++ src/app/customer/service/customer.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserStorageService } from '../../storoge/user-storage.service';
+
+const BASIC_URL = "http://localhost:8080/";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllProducts(): Observable<any> {
+      return this.http.get(BASIC_URL + 'api/customer/products', {
+        headers: this.createAuthorizationHeader(),
+      });
+    }
+  
+    getAllProductsByName(name: any): Observable<any> {
+    return this.http.get(BASIC_URL+`api/customer/search/${name}`, {
+      headers: this.createAuthorizationHeader(),
+    });
+  }
+
+  private createAuthorizationHeader(): HttpHeaders {
+      return new HttpHeaders().set(
+          'Authorization', 'Bearer ' + UserStorageService.getToken()
+      );
+  }
 }
